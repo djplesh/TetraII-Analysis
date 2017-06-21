07f94cfad746b21d5fad71777ed8e4f322723ee6
jonah-h <jhh451@gmail.com>
2017-06-21 15:25:56 +0000

    added exceptions and icons

--- exceptions.py
+++ exceptions.py
@@ -5,4 +5,15 @@ Created on Tue Jun 20 19:43:15 2017
 @author: Jonah Hoffman
 """
 
-class 
+
+class NonexistantFile(Exception):
+    def __init__(self, file_name):
+        self.args = ('No file "%s" found' % file_name,)
+
+
+try:
+    raise NonexistantFile('yo')
+except Exception as inst:
+    print(type(inst))    # the exception instance
+    print(inst.args)     # arguments stored in .args
+    print(inst)
